853fd8f40734339825f594e39ca596742b9c0b52
Andy Walter <musictopia2@hotmail.com>
2026-03-28 10:32:44 -0700

    Add project files.

---
+++ GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.BuildHooks;
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿await BuildHookRunner.RunAsync(args, async payLoad =>
+{
+
+    Console.WriteLine(payLoad);
+    //this is used to begin with which is the beginnings of seeing what i need for this.
+
+
+});
